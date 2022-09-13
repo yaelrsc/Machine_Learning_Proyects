4191c45d87a9d9ed5291e35196cb229c9bfae041
Yael313255748 <yaelrsc@ciencias.unam.mx>
2022-09-13 16:33:33 -0500

    Cambios Realizados

--- Classification_Models/Machine_Learning_Tools/sklearn_models.py
+++ Classification_Models/Machine_Learning_Tools/sklearn_models.py
@@ -3,124 +3,10 @@ from sklearn import clone
 from pandas import DataFrame
 from numpy import zeros
 from sklearn.pipeline import Pipeline
-from tensorflow.keras import (layers,Model,Input)
-from tensorflow.keras.backend import clear_session
-
-def single_layer_perceptron(input_shape,n_outputs,optimizer,loss,metrics=None,**kwargs):
-    
-    clear_session()
-    
-    inputs = Input(shape=input_shape)
-    outputs = layers.Dense(n_outputs,**kwargs)(inputs)
-    
-    model = Model(inputs=inputs,outputs=outputs)
-    
-    model.compile(optimizer,loss,metrics)
-    
-    return model
-
-def multi_layer_perceptron(input_shape,n_outputs,n_neurons,n_hidden_layers,act,out_act,
-                           optimizer,loss,metrics=None,dropout=False,batch_normalization=False,
-                           dropout_rate=None,momentum_bn=None,epsilon_bn=None,**kwargs):
-    
-    clear_session()
-    
-    inputs = Input(shape=input_shape)
-    
-    for i in range(n_hidden_layers):
-        
-        if i == 0:
-            
-            x = layers.Dense(n_neurons,activation=act,**kwargs)(inputs)
-            
-        else:
-            
-            x = layers.Dense(n_neurons,activation=act,**kwargs)(x)
-        
-        if dropout:
-            
-            x = layers.Dropout(dropout_rate)(x)
-            
-        if batch_normalization:
-            
-            x = layers.BatchNormalization(momentum=momentum_bn,epsilon=epsilon_bn)(x)
-            
-        
-        
-    outputs = layers.Dense(n_outputs,activation=out_act,**kwargs)(x)
-    
-    model = Model(inputs=inputs,outputs=outputs)
-    
-    model.compile(optimizer,loss,metrics)
-    
-    return model
-
-def convolutinal_neural_network(input_shape,n_outputs,n_conv_maxpol_layers=2,n_filters=50,kernel_size=(2,2),strides=(1,1),
-                                padding='valid',pool_size=(2,2),pool_strides=None,n_neurons=100,n_hidden_layers=2,act='relu',
-                                out_act='softmax',optimizer=None,loss=None,metrics=None,dropout=False,batch_normalization=False,
-                                dropout_rate=None,momentum_bn=None,epsilon_bn=None,pool_kind='MAX',**kwargs):
-    
-    clear_session()
-    
-    inputs = Input(shape=input_shape)
-    
-    for k in range(n_conv_maxpol_layers):
-        
-        if k == 0:
-            
-            x = layers.Conv2D(n_filters,kernel_size,strides,padding,**kwargs)(inputs)
-            
-        else:
-            
-            x = layers.Conv2D(n_filters,kernel_size,strides,padding,**kwargs)(x)
-        
-        if pool_kind == 'MAX':
-            
-            x = layers.MaxPooling2D(pool_size,pool_strides,padding)(x)
-        
-        elif pool_kind == 'AVG':
-            
-            x = layers.AveragePooling2D(pool_size,pool_strides,padding)(x)
-            
-    
-        x = layers.Activation(act)(x)
-        
-        if dropout:
-            
-            x = layers.Dropout(dropout_rate)(x)
-            
-        if batch_normalization:
-            
-            x = layers.BatchNormalization(momentum=momentum_bn,epsilon=epsilon_bn)(x)
-        
-    x = layers.Flatten()(x)
-        
-        
-    for i in range(n_hidden_layers):
-        
-        x = layers.Dense(n_neurons,activation=act,**kwargs)(x)
-        
-        if dropout:
-            
-            x = layers.Dropout(dropout_rate)(x)
-            
-        if batch_normalization:
-            
-            x = layers.BatchNormalization(momentum=momentum_bn,epsilon=epsilon_bn)(x)
-                 
-        
-    outputs = layers.Dense(n_outputs,activation=out_act,**kwargs)(x)
-    
-    model = Model(inputs=inputs,outputs=outputs)
-    
-    model.compile(optimizer,loss,metrics)
-    
-    return model
-    
 
 class Supervised_Model():
     
-    def __init__(self,data,features,target,model,model_name='model'):
+    def __init__(self,data,features,target,model=None,model_name='model'):
         
         self.data = data
         self.target = target
@@ -130,6 +16,11 @@ class Supervised_Model():
         self.model = model
         self.model_name = model_name
         self.steps = []
+    
+    def add_model(self,model,model_name='model'):
+        
+        self.model = model
+        self.model_name = model_name
         
     def add_column_transformer(self,transformer,name='column_transformer'):
         
